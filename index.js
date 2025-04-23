f885a4cdf1e939c7b29febb1bb34302281cf5661
shaharmaliss <111096205+shaharmaliss@users.noreply.github.com>
2025-04-23 19:56:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/:eventCode/:cardCode" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        {/* Updated route to accept two dynamic parameters */}
+        <Route path="/:eventCode/:cardCode" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
+
+reportWebVitals();

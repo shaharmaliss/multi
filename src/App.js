aad24b2bba4a7b2eba9237460f77d294f912faaf
shaharmaliss <111096205+shaharmaliss@users.noreply.github.com>
2025-06-02 23:30:28 +0300

    added new page - Mobil numbers validation
    name of page: new-card-2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ExercisePage from './exercise-page';
 import NewRemark from './new-remark';
 import NewCard from './new-card';
+import SubscribePage from './new-card-2';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/:eventCode/:cardCode" element={<ExercisePage />} />
         <Route path="/new-remark" element={<NewRemark />} />
         <Route path="/new-card" element={<NewCard />} />
+        <Route path="/new-card-2" element={<SubscribePage />} />
       </Routes>
     </Router>
   );
